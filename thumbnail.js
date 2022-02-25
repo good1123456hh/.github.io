bf658195f51ba227531b74754d54254a33e753ba
good1123456hh <100208228+good1123456hh@users.noreply.github.com>
2022-02-25 20:13:43 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function(event) {
+   
+let targetImage = document.querySelector("#smart-image");
+targetImage.classList.remove("small");})
+ 
+ 
+ 
+ targetImage.addEventListener("click", function() {alert("点击了图片");});
+ 
